cb6c404c987b41b6d000adfe3b86b6d4b8336ae2
pibc <pdbuczkowski@gmail.com>
2021-05-02 23:23:36 +0200

    Login to Demo. WIP: NetworkOnMainThreadException

--- app/src/main/java/code/solutions/myfx/ui/login/LoginViewModel.kt
+++ app/src/main/java/code/solutions/myfx/ui/login/LoginViewModel.kt
@@ -2,9 +2,12 @@ package code.solutions.myfx.ui.login
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import code.solutions.fxmodel.Myfx
 
 class LoginViewModel : ViewModel() {
     fun onClickedLoginButton(login: String, password: String) {
         Log.v("LOGIN", "onClickedLoginButton($login, $password)")
+        val myfx = Myfx()
+        myfx.login(login, password)
     }
 }
--- fxmodel/src/main/java/code/solutions/fxmodel/Myfx.kt
+++ fxmodel/src/main/java/code/solutions/fxmodel/Myfx.kt
@@ -1,4 +1,39 @@
 package code.solutions.fxmodel
 
+import com.dukascopy.api.Instrument
+import com.dukascopy.api.system.ClientFactory
+import com.dukascopy.api.system.IClient
+import com.dukascopy.api.system.ISystemListener
+import org.slf4j.LoggerFactory
+
 class Myfx {
+    private val client: IClient = ClientFactory.getDefaultInstance()
+    private val logger = LoggerFactory.getLogger("Myfx")
+    private val jnlpLive = "https://platform.dukascopy.com/live_3/jforex_3.jnlp"
+    private val jnlpDemo = "https://platform.dukascopy.com/demo/jforex.jnlp"
+
+    init {
+        client.setSystemListener(object : ISystemListener {
+            override fun onStart(processId: Long) {
+                logger.debug("onStart(processId=$processId")
+            }
+
+            override fun onStop(processId: Long) {
+                logger.debug("onStop(processId=$processId)")
+            }
+
+            override fun onConnect() {
+                logger.debug("onConnect()")
+                client.subscribedInstruments = setOf(Instrument.EURUSD)
+            }
+
+            override fun onDisconnect() {
+                logger.debug("onDisconnect()")
+            }
+        })
+    }
+
+    fun login(login: String, password: String) {
+        client.connect(jnlpDemo, login, password)
+    }
 }
