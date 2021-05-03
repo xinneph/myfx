0f5e36c680934df3b01d79118b0149851f038bee
pibc <pdbuczkowski@gmail.com>
2021-05-03 16:55:29 +0200

    Requesting login on IO thread

--- app/src/main/java/code/solutions/myfx/ui/login/LoginViewModel.kt
+++ app/src/main/java/code/solutions/myfx/ui/login/LoginViewModel.kt
@@ -2,12 +2,17 @@ package code.solutions.myfx.ui.login
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import code.solutions.fxmodel.Myfx
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class LoginViewModel : ViewModel() {
     fun onClickedLoginButton(login: String, password: String) {
-        Log.v("LOGIN", "onClickedLoginButton($login, $password)")
-        val myfx = Myfx()
-        myfx.login(login, password)
+        viewModelScope.launch(Dispatchers.IO) {
+            Log.v("LOGIN", "onClickedLoginButton($login, $password)")
+            val myfx = Myfx()
+            myfx.login(login, password)
+        }
     }
 }
